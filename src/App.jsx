724c94fc1c6c90154e967e5a66a858503ad928b3
umargafia <77011124+umargafia@users.noreply.github.com>
2022-10-04 14:23:01 +0100

    starting the header section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Nav } from "./components/navigation/Nav";
+import "./global/global.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <section className="sections">
+        <Nav/>
+     </section>
+    </div>
+  );
+}
+
+export default App;
--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,5 +1,37 @@
 import "./nav.scss";
 
 export const Nav = () => {
-  return <div className="nav">nav</div>;
+  return (
+    <div className="nav">
+      <div className="nav__intro">
+        <div className="nav__intro--img">Logo</div>
+        <div className="nav__intro--header">
+          <h1 className="heading--primary">Proxine</h1>
+          <span className="subHeader--text">We bring the best experience</span>
+        </div>
+      </div>
+      <div className="nav__navigation">
+        <div className="nav__navigation--header">
+          <ul className="list">
+            <li className="list--item">
+              <a href="#" className="link">
+                Login
+              </a>
+            </li>
+            <li className="list--item">
+              <a href="#" className="link">
+                Flight
+              </a>
+            </li>
+            <li className="list--item">
+              <a href="#" className="link">
+                {" "}
+                contact us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
